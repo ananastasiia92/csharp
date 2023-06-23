29c062eb641e32f32a25a56b38136141dcbe0767
Anastasia <talanovanastya@gmail.com>
2023-06-23 19:58:58 +0300

    expl004

--- exeples/expl003/Program.cs
+++ exeples/expl003/Program.cs
@@ -1,3 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿double numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);
+
